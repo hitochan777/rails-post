7580d2c052a1ac7f54c5d98e6d3438681ab7d172
Otsuki Hitoshi <hitochan777@gmail.com>
2019-05-26 06:06:25 +0900

    Finish user signup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
 
@@ -9,5 +10,8 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def hello
   end
 end
