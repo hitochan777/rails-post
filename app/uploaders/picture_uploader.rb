2eb4e3756ac530c25cf004db64c9640c753d4b0f
Otsuki Hitoshi <hitochan777@gmail.com>
2019-06-02 18:03:49 +0900

    Add user microposts

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,16 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
